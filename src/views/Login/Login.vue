f8db921b8efecc1f2c2ef544ba4741c580d7d248
dongdong <m18705620350@163.com>
2018-12-23 14:00:13 +0800

    完成轮播图和登陆页面的几个小问题

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -4,16 +4,16 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a href="javascript:;" :class="{on:loginTypes}" @click="loginTypes=true">短信登录</a>
+          <a href="javascript:;" :class="{on:!loginTypes}" @click="loginTypes=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on:loginTypes}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!isPhone || computeTime > 0" class="get_verification" :class="{right_phone_number:isPhone && !computeTime}" @click.prevent="sendCode">{{computeTime>0?`已发送(${computeTime})s`:'发送验证码'}}</button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -23,16 +23,16 @@
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on:!loginTypes}">
             <section>
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input :type="!isShowPwd?'text':'password'" maxlength="8" placeholder="密码">
+                <div class="switch_button" :class="!isShowPwd?'off':'on'" @click="isShowPwd = !isShowPwd">
+                  <div class="switch_circle" :class="!isShowPwd?'':'right'"></div>
+                  <span class="switch_text">{{!isShowPwd?'':'abc'}}</span>
                 </div>
               </section>
               <section class="login_message">
@@ -56,7 +56,29 @@
   export default {
     name: '',
     data () {
-      return {}
+      return {
+        loginTypes: true, //true短信登录 false 密码登录
+        phone:'',
+        computeTime: 0,
+        isShowPwd: false
+      }
+    },
+    computed: {
+      isPhone () {
+        const {phone} = this
+        return /^1\d{10}$/.test(phone)
+      }
+    },
+    methods: {
+      sendCode () {
+        this.computeTime = 30
+        const intervalId = setInterval(() => {
+          if (this.computeTime <= 0) {
+            clearInterval(intervalId)
+          }
+          this.computeTime--
+        },1000)
+      }
     }
   }
 </script>
@@ -122,6 +144,8 @@
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right_phone_number
+                  color #000
             .login_verification
               position relative
               margin-top 16px
@@ -161,6 +185,8 @@
                   background #fff
                   box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                   transition transform .3s
+                  &.right
+                    transform translateX(27px)
             .login_hint
               margin-top 12px
               color #999
--- src/views/MSite/MSite.vue
+++ src/views/MSite/MSite.vue
@@ -11,112 +11,21 @@
     </NavHeader>
     <!--首页导航-->
     <nav class="msite_nav">
-      <div class="swiper-container">
+      <div class="swiper-container" v-if="categorys.length>0">
         <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <a href="javascript:" class="link_to_food">
+          <div class="swiper-slide" v-for="(categorys,index) in categorysArr" :key="index">
+            <a href="javascript:" class="link_to_food" v-for="(c,index) in categorys" :key="index">
               <div class="food_container">
-                <img src="./images/nav/1.jpg">
+                <img :src="'https://fuss10.elemecdn.com' + c.image_url">
               </div>
-              <span>甜品饮品</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/2.jpg">
-              </div>
-              <span>商超便利</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/3.jpg">
-              </div>
-              <span>美食</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/4.jpg">
-              </div>
-              <span>简餐</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/5.jpg">
-              </div>
-              <span>新店特惠</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/6.jpg">
-              </div>
-              <span>准时达</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/7.jpg">
-              </div>
-              <span>预订早餐</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/8.jpg">
-              </div>
-              <span>土豪推荐</span>
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/9.jpg">
-              </div>
-              <span>甜品饮品</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/10.jpg">
-              </div>
-              <span>商超便利</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/11.jpg">
-              </div>
-              <span>美食</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/12.jpg">
-              </div>
-              <span>简餐</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/13.jpg">
-              </div>
-              <span>新店特惠</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/14.jpg">
-              </div>
-              <span>准时达</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/1.jpg">
-              </div>
-              <span>预订早餐</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/2.jpg">
-              </div>
-              <span>土豪推荐</span>
+              <span>{{c.title}}</span>
             </a>
           </div>
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
       </div>
+      <img src="./images/msite_back.svg" v-else>
     </nav>
     <!--首页附近商家-->
     <div class="msite_shop_list">
@@ -130,6 +39,8 @@
 </template>
 
 <script>
+  import Swiper from 'swiper'
+  import 'swiper/dist/css/swiper.min.css'
   import ShopList from '../../components/ShopList/ShopList.vue'
   import {mapState} from 'vuex'
 
@@ -138,9 +49,38 @@
     mounted () {
       this.$store.dispatch('getAddress')
       this.$store.dispatch('getShops')
+      this.$store.dispatch('getCategorys')
     },
     computed: {
-      ...mapState(['address'])
+      ...mapState(['address','categorys']),
+      categorysArr () {
+        const bigArr = []
+        let smallArr = []
+        const {categorys} = this
+        categorys.forEach(c => {
+          if (smallArr.length === 0) {
+            bigArr.push(smallArr)
+          }
+          smallArr.push(c)
+          if (smallArr.length === 8) {
+            smallArr = []
+          }
+        })
+        return bigArr
+      }
+    },
+    watch: {
+      categorys () {
+        this.$nextTick(() => {
+          new Swiper ('.swiper-container', {
+            loop: true, // 循环模式选项
+            // 如果需要分页器
+            pagination: {
+              el: '.swiper-pagination',
+            }
+          })
+        })
+      }
     },
     components: {
       ShopList,
